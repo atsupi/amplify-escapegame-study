8681fe87447cc38810d8ebdcc28a7a4c58e0ecc5
EC2 Default User <ec2-user@ip-172-31-8-96.ap-northeast-1.compute.internal>
2022-09-20 18:32:52 +0000

    Initial commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  devServer: {
+      client: {
+          webSocketURL: 'ws://0.0.0.0:8080/ws',
+      },
+       allowedHosts: 'all',
+  },
   transpileDependencies: true
 })
